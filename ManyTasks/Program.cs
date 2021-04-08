187ba0e5ae6260e9c0dd4b0c74f1d79af5f66c25
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-08 22:44:02 +0300

    Add project files.

---
+++ ManyTasks/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformanceCreate();
+        }
+
+        static void Create()
+        {
+            var tasks = new List<Task>();
+            var stopWatch = new Stopwatch();
+
+            for (int i = 0; i < 30; i++)
+            {
+                stopWatch.Start();
+                for (int j = 0; j < 10_000_000; j++)
+                {
+                    tasks.Add(Task.Factory.StartNew(() =>
+                    {
+                        Thread.Sleep(100);
+                    }));
+                }
+                stopWatch.Stop();
+                double second = stopWatch.ElapsedMilliseconds;
+                Console.WriteLine($"{second}\t\t{tasks.Count}");
+                stopWatch.Reset();
+            } // 3-4 seconds for created 10 000 000 threads (16gb i5)
+
+            Task.WaitAll(tasks.ToArray()); // it is not optimal!
+        }
+
+        static void PerformanceCreate()
+        {
+            var createTimeTasks = new Stopwatch();
+            var collectTime = new Stopwatch();
+            int countTasks = 0;
+
+            //GC.AddMemoryPressure(999999999999); // ?
+
+            for (int i = 0; i < 1_000; i++)
+            {
+                createTimeTasks.Start();
+                for (int j = 0; j < 1_000_000; j++)
+                {
+                    var t = Task.Factory.StartNew(() =>
+                    {
+                        ;
+                    });
+                }
+                createTimeTasks.Stop();
+                countTasks++;
+
+                Thread.Sleep(10000);
+
+                collectTime.Start();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                collectTime.Stop();
+
+                //Console.WriteLine($"{createTimeTasks.ElapsedMilliseconds}\t\t{countTasks}\t\t{GC.GetTotalMemory(false)}");
+                Console.WriteLine($"{collectTime.ElapsedMilliseconds}\t\t{createTimeTasks.ElapsedMilliseconds}\t\t{countTasks}\t\t{GC.GetTotalMemory(false)}");
+                createTimeTasks.Reset();
+                collectTime.Reset();
+            }
+            // how wait ???
+
+            // 217 million task
+        }
+    }
+}
---
+++ Research/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ManyTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
