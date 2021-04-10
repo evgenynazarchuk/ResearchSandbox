e21fbccc0d5cde4894f06922da766278519f05a9
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-10 19:31:14 +0300

    may be call

---
+++ AnyOf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MayBeCallApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MayBeCall(0.5, () =>
+            {
+                Func1("func1");
+            });
+        }
+
+        public static void Func1(string msg) => Console.WriteLine(msg);
+
+        public static void MayBeCall(double probability, Action act)
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            if (probability > random.NextDouble())
+            {
+                act();
+            }
+        }
+    }
+}
