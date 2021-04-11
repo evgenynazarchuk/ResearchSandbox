93a9c9034b2ae0de958bfabbc7847928c95d9220
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-11 17:27:29 +0300

    check code

--- ValueTaskSamples/Program.cs
+++ ValueTaskSamples/Program.cs
@@ -7,64 +7,85 @@ namespace ValueTaskSamples
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            // это параллельное создание тасок
-            // нужно изучить почему падает скорость из за использования Task.Delay
-            // и как это избежать
-            // 100 за 1.5 секунды
-            CheckGenerateSimpleTaskAndValueTask(1, 1, 100);
-            CheckGenerateSimpleAsyncTaskAndValueTask(1, 1, 100);
-            
-
-            //Console.WriteLine($"{await CreateAsyncValueTask()}");
-            //Console.WriteLine($"{await CreateAsyncTask()}");
-            //Console.WriteLine($"{CreateAsyncValueTask().AsTask().GetAwaiter().GetResult()}");
-            //Console.WriteLine($"{CreateAsyncTask().GetAwaiter().GetResult()}");
+            CheckGenerateSimpleAsyncTaskAndValueTask(10, 10, 1000000);
         }
 
         public static void CheckGenerateSimpleAsyncTaskAndValueTask(int k, int l, int m)
         {
+            var valueTasks = new Task[k];
+            var tasks = new Task[k];
+
             for (int i = 0; i < k; i++)
             {
-                GenerateSimpleAsyncValueTask(l, m).Wait();
-                GenerateSimpleAsyncTask(l, m).Wait();
+                valueTasks[i] = GenerateSimpleAsyncValueTask(l, m);
+                tasks[i] = GenerateSimpleAsyncTask(l, m);
+                Console.WriteLine("--------------");
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                valueTasks[i].Wait();
+                tasks[i].Wait();
                 Console.WriteLine("--------------");
             }
         }
 
         public static async Task GenerateSimpleAsyncValueTask(int n, int m)
         {
-            Stopwatch watch = new();
+            var watch = new Stopwatch();
+            var tasks = new ValueTask<int>[n, m];
+            var result = 0;
 
-            int result = 0;
             watch.Start();
-            for (int i = 0; i < n; i++)
+            for (var i = 0; i < n; i++)
             {
-                for (int j = 0; j < m; j++)
+                for (var j = 0; j < m; j++)
                 {
-                    result += await CreateAsyncValueTask();
+                    tasks[i, j] = CreateAsyncValueTask();
+                }
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    result += tasks[i, j].Result;
                 }
             }
             watch.Stop();
+
             Console.WriteLine($"Result: {result}, Value time: {watch.ElapsedMilliseconds}");
+            await Task.CompletedTask;
         }
 
         public static async Task GenerateSimpleAsyncTask(int n, int m)
         {
-            Stopwatch watch = new();
+            var watch = new Stopwatch();
+            var tasks = new Task<int>[n, m];
+            var result = 0;
 
-            int result = 0;
             watch.Start();
-            for (int i = 0; i < n; i++)
+            for (var i = 0; i < n; i++)
             {
-                for (int j = 0; j < m; j++)
+                for (var j = 0; j < m; j++)
                 {
-                    result += await CreateAsyncTask();
+                    tasks[i, j] = CreateAsyncTask();
+                }
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    result += tasks[i, j].Result;
                 }
             }
             watch.Stop();
+
             Console.WriteLine($"Result: {result}, Task time: {watch.ElapsedMilliseconds}");
+            await Task.CompletedTask;
         }
 
         public static async ValueTask<int> CreateAsyncValueTask()
@@ -77,62 +98,6 @@ namespace ValueTaskSamples
         {
             await Task.Delay(1);
             return 11;
-        }
-
-        public static void CheckGenerateSimpleTaskAndValueTask(int k, int l, int m)
-        {
-            for (int i = 0; i < k; i++)
-            {
-                GenerateSimpleValueTask(l, m).Wait();
-                GenerateSimpleTask(l, m).Wait();
-                Console.WriteLine("--------------");
-            }
-        }
-
-        public static async Task GenerateSimpleValueTask(int n, int m)
-        {
-            Stopwatch watch = new();
-
-            int result = 0;
-            watch.Start();
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    result += await CreateValueTask();
-                }
-            }
-            watch.Stop();
-            Console.WriteLine($"Result: {result}, Value time: {watch.ElapsedMilliseconds}");
-        }
-
-        public static async Task GenerateSimpleTask(int n, int m)
-        {
-            Stopwatch watch = new();
-
-            int result = 0;
-            watch.Start();
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    result += await CreateTask();
-                }
-            }
-            watch.Stop();
-            Console.WriteLine($"Result: {result}, Task time: {watch.ElapsedMilliseconds}");
-        }
-
-        public static ValueTask<int> CreateValueTask()
-        {
-            Task.Delay(1).Wait();
-            return new(11);
-        }
-
-        public static Task<int> CreateTask()
-        {
-            Task.Delay(1).Wait();
-            return Task.FromResult(11);
         }
     }
 }
