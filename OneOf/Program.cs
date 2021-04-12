54e923a40176581e943df064e2e69d0ed023a995
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-12 21:55:02 +0300

    add waiters

---
+++ OneOf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OneOf
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                OneOf(
+                    Func1,
+                    Func2,
+                    () => Func3(42)
+                );
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                OneOf(
+                    () => Func4(i)
+                );
+            }
+        }
+
+        public static void OneOf(params Action[] actions)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(0, actions.Length);
+            actions[randomNumber]();
+        }
+
+        public static void Func1() => Console.WriteLine($"1");
+        public static void Func2() => Console.WriteLine($"2");
+        public static void Func3(int n) => Console.WriteLine($"{n}");
+        public static void Func4(int i) => Console.WriteLine($"4: {i}");
+    }
+}
---
+++ RandomTimer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Trace(() => Wait(3.Seconds()));
+                Trace(() => WaitMaximum(5.Seconds()));
+                Trace(() => WaitMinimum(3.Seconds(), 7.Seconds()));
+                Trace(() => WaitToRun(3.Seconds(), () => { }));
+                Trace(() => WaitToRun(3.Seconds(), 7.Seconds(), () => { }));
+                Console.WriteLine($"-----------------");
+            }
+        }
+
+        public static void Trace(Action action)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            action();
+            watch.Stop();
+            Console.WriteLine($"{watch.ElapsedMilliseconds}");
+        }
+
+        public static void Wait(TimeSpan time) => Task.Delay(time).GetAwaiter().GetResult();
+
+        public static void WaitMaximum(TimeSpan maximumTime)
+        {
+            var random = new Random();
+            var wait = random.Next(0, (int)maximumTime.TotalMilliseconds);
+            Task.Delay(wait).GetAwaiter().GetResult();
+        }
+
+        public static void WaitMinimum(TimeSpan minimumTime, TimeSpan maximumTime)
+        {
+            var random = new Random();
+            var wait = random.Next((int)minimumTime.TotalMilliseconds, (int)maximumTime.TotalMilliseconds);
+            Task.Delay(wait).GetAwaiter().GetResult();
+        }
+
+        public static void WaitToRun(TimeSpan maximumTime, Action action)
+        {
+            WaitMaximum(maximumTime);
+            action();
+        }
+
+        public static void WaitToRun(TimeSpan minimumTime, TimeSpan  maximum, Action action)
+        {
+            WaitMinimum(minimumTime, maximum);
+            action();
+        }
+    }
+
+    public static class IntExt
+    {
+        public static TimeSpan Seconds(this int n)
+        {
+            return TimeSpan.FromSeconds(n);
+        }
+    }
+}
