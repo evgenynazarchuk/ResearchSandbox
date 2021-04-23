6f998ef1b7b55e0f9be88410d2638a1d866c3076
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-23 15:19:54 +0300

    add json reader

---
+++ ReadJsonFile/LocationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJsonFile
+{
+    public class LocationDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NameLocal { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}" + " " + Name + " " + NameLocal;
+        }
+    }
+}
---
+++ ReadJsonFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text;
+
+namespace ReadJsonFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            ReadJson();
+        }
+
+        public static void ReadJson()
+        {
+            const string path = "LocationJsonFile.json";
+            var stream = new StreamReader(path);
+            string line;
+
+            while ((line = stream.ReadLine()) != null)
+            {
+                var opt = new JsonSerializerOptions();
+                opt.PropertyNameCaseInsensitive = true; // не учитываем регистр атрибутов
+
+                var obj = JsonSerializer.Deserialize<LocationDto>(line, opt);
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine(obj);
+            }
+
+            stream.Close();
+        }
+    }
+}
