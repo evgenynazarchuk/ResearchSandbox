d426ffa7cbc1792caf70bfc1516ee785705a1607
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-24 21:22:17 +0300

    add code

---
+++ ReportWriter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReportWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ReportWriter/ReportWriter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.IO;
+
+namespace ReportWriter
+{
+    public class ReportWriter
+    {
+        private readonly StreamWriter _writer;
+
+        public ReportWriter(string path)
+        {
+            this._writer = new(path, false, Encoding.UTF8, 65535);
+        }
+
+        public void Write(string msg)
+        {
+            this._writer.WriteLine(msg);
+            this._writer.Flush();
+        }
+
+        public void Dispose()
+        {
+            this._writer.Close();
+        }
+    }
+}
