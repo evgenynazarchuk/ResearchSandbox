1fea8949706bd3e157a6dca9482023138cae516a
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-24 20:49:59 +0300

    code style

--- ReadCsvInDataTable/CsvModel.cs
+++ ReadCsvInDataTable/CsvModel.cs
@@ -6,244 +6,76 @@ namespace CsvReader
     {
         public abstract void InitializationFromRow(ReadOnlySpan<string> row);
 
+        //
         public static int IntegerIsRequired(ReadOnlySpan<char> row)
-        {
-            if (int.TryParse(row, out int result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Integer field is required");
-            }
-        }
-
-        public static int? IntegerIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (int.TryParse(row, out int result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => int.TryParse(row, out int result) ? result 
+            : throw new ApplicationException("Integer field is required");
 
         public static uint UnsignedIntegerIsRequired(ReadOnlySpan<char> row)
-        {
-            if (uint.TryParse(row, out uint result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Unsigned Integer field is required");
-            }
-        }
-
-        public static uint? UnsignedIntegerIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (uint.TryParse(row, out uint result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => uint.TryParse(row, out uint result) ? result 
+            : throw new ApplicationException("Unsigned Integer field is required");
 
         public static long LongIntegerIsRequired(ReadOnlySpan<char> row)
-        {
-            if (long.TryParse(row, out long result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Long Integer field is required");
-            }
-        }
-
-        public static long? LongIntegerIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (long.TryParse(row, out long result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => long.TryParse(row, out long result) ? result 
+            : throw new ApplicationException("Long Integer field is required");
 
         public static ulong UnsignedLongIntegerIsRequired(ReadOnlySpan<char> row)
-        {
-            if (ulong.TryParse(row, out ulong result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Long Integer field is required");
-            }
-        }
-
-        public static ulong? UnsignedLongIntegerIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (ulong.TryParse(row, out ulong result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => ulong.TryParse(row, out ulong result) ? result 
+            : throw new ApplicationException("Long Integer field is required");
 
         public static float FloatIsRequired(ReadOnlySpan<char> row)
-        {
-            if (float.TryParse(row, out float result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Float field is required");
-            }
-        }
-
-        public static float? FloatIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (float.TryParse(row, out float result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
+            => float.TryParse(row, out float result) ? result 
+            : throw new ApplicationException("Float field is required");
+        
         public static double DoubleIsRequired(ReadOnlySpan<char> row)
-        {
-            if (double.TryParse(row, out double result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Double field is required");
-            }
-        }
-
-        public static double? DoubleIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (double.TryParse(row, out double result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
+            => double.TryParse(row, out double result) ? result
+            : throw new ApplicationException("Double field is required");
+        
         public static decimal DecimalIsRequired(ReadOnlySpan<char> row)
-        {
-            if (decimal.TryParse(row, out decimal result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Decimal field is required");
-            }
-        }
-
-        public static decimal? DecimalIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (decimal.TryParse(row, out decimal result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => decimal.TryParse(row, out decimal result) ? result
+            : throw new ApplicationException("Decimal field is required");
 
         public static bool BooleanIsRequered(ReadOnlySpan<char> row)
-        {
-            if (bool.TryParse(row, out bool result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("Boolean field is required");
-            }
-        }
-
-        public static bool? BooleanIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (bool.TryParse(row, out bool result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => bool.TryParse(row, out bool result) ? result
+            : throw new ApplicationException("Boolean field is required");
 
         public static DateTime DateTimeIsRequered(ReadOnlySpan<char> row)
-        {
-            if (DateTime.TryParse(row, out DateTime result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("DateTime field is required");
-            }
-        }
-
-        public static DateTime? DateTimeIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (DateTime.TryParse(row, out DateTime result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => DateTime.TryParse(row, out DateTime result) ? result
+            : throw new ApplicationException("DateTime field is required");
 
         public static TimeSpan TimeSpanIsRequered(ReadOnlySpan<char> row)
-        {
-            if (TimeSpan.TryParse(row, out TimeSpan result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ApplicationException("DateTime field is required");
-            }
-        }
+            => TimeSpan.TryParse(row, out TimeSpan result) ? result
+            : throw new ApplicationException("DateTime field is required");
+
+        //
+        public static int? IntegerIsNotRequired(ReadOnlySpan<char> row)
+            => int.TryParse(row, out int result) ? result : null;
+
+        public static uint? UnsignedIntegerIsNotRequired(ReadOnlySpan<char> row)
+            => uint.TryParse(row, out uint result) ? result : null;
+
+        public static long? LongIntegerIsNotRequired(ReadOnlySpan<char> row)
+            => long.TryParse(row, out long result) ? result : null;
+
+        public static ulong? UnsignedLongIntegerIsNotRequired(ReadOnlySpan<char> row)
+            => ulong.TryParse(row, out ulong result) ? result : null;
+
+        public static float? FloatIsNotRequired(ReadOnlySpan<char> row)
+            => float.TryParse(row, out float result) ? result : null;
+
+        public static double? DoubleIsNotRequired(ReadOnlySpan<char> row)
+            => double.TryParse(row, out double result) ? result : null;
+
+        public static decimal? DecimalIsNotRequired(ReadOnlySpan<char> row)
+            => decimal.TryParse(row, out decimal result) ? result : null;
+
+        public static bool? BooleanIsNotRequired(ReadOnlySpan<char> row)
+            => bool.TryParse(row, out bool result) ? result : null;
+
+        public static DateTime? DateTimeIsNotRequired(ReadOnlySpan<char> row)
+            => DateTime.TryParse(row, out DateTime result) ? result : null;
 
         public static TimeSpan? TimeSpanIsNotRequired(ReadOnlySpan<char> row)
-        {
-            if (TimeSpan.TryParse(row, out TimeSpan result))
-            {
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
+            => TimeSpan.TryParse(row, out TimeSpan result) ? result : null;
     }
 }
