da1ec7ba3f449a1e06ee385450a70211c8ff369f
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-04-26 10:27:47 +0300

    fix defect - izbasar musagulov forever tester =)

--- ReadCsvInDataTable/CsvModel.cs
+++ ReadCsvInDataTable/CsvModel.cs
@@ -49,33 +49,116 @@ namespace CsvReader
 
         //
         public static int? IntegerIsNotRequired(ReadOnlySpan<char> column)
-            => int.TryParse(column, out int columnValue) ? columnValue : null;
+        {
+            if (column.Length > 0)
+            {
+                return int.TryParse(column, out int columnValue) 
+                    ? columnValue 
+                    : throw new CsvModelException("Integer must be integer");
+            }
+            return null;
+        }
 
         public static uint? UnsignedIntegerIsNotRequired(ReadOnlySpan<char> column)
-            => uint.TryParse(column, out uint columnValue) ? columnValue : null;
+        {
+            if (column.Length > 0)
+            {
+                return uint.TryParse(column, out uint columnValue) ? columnValue
+                    : throw new CsvModelException("Unsigned Integer must be unsigned integer");
+            }
+            return null;
+        }
 
         public static long? LongIntegerIsNotRequired(ReadOnlySpan<char> column)
-            => long.TryParse(column, out long columnValue) ? columnValue : null;
-
+        {
+            if (column.Length > 0)
+            {
+                return long.TryParse(column, out long columnValue) ? columnValue
+                    : throw new CsvModelException("Long Integer must be long integer");
+            }
+            return null;
+        }
+        
         public static ulong? UnsignedLongIntegerIsNotRequired(ReadOnlySpan<char> column)
-            => ulong.TryParse(column, out ulong columnValue) ? columnValue : null;
-
+        {
+            if (column.Length > 0)
+            {
+                return ulong.TryParse(column, out ulong columnValue) ? columnValue
+                    : throw new CsvModelException("Unsigned Long Integer must be unsigned long integer");
+            }
+            return null;
+        }
+        
         public static float? FloatIsNotRequired(ReadOnlySpan<char> column)
-            => float.TryParse(column, out float columnValue) ? columnValue : null;
+        {
+            if (column.Length > 0)
+            {
+                return float.TryParse(column, out float columnValue) ? columnValue
+                    : throw new CsvModelException("Float must be float");
+            }
+            return null;
+        }
 
         public static double? DoubleIsNotRequired(ReadOnlySpan<char> column)
-            => double.TryParse(column, out double columnValue) ? columnValue : null;
+        {
+            {
+                if (column.Length > 0)
+                {
+                    return double.TryParse(column, out double columnValue) ? columnValue
+                        : throw new CsvModelException("Doouble must be double");
+                }
+                return null;
+            }
+        }
 
         public static decimal? DecimalIsNotRequired(ReadOnlySpan<char> column)
-            => decimal.TryParse(column, out decimal columnValue) ? columnValue : null;
+        {
+            {
+                if (column.Length > 0)
+                {
+                    return decimal.TryParse(column, out decimal columnValue) ? columnValue
+                        : throw new CsvModelException("Decimal must be decimal");
+                }
+                return null;
+            }
+        }
+        
 
         public static bool? BooleanIsNotRequired(ReadOnlySpan<char> column)
-            => bool.TryParse(column, out bool columnValue) ? columnValue : null;
+        {
+            {
+                if (column.Length > 0)
+                {
+                    return bool.TryParse(column, out bool columnValue) ? columnValue
+                        : throw new CsvModelException("Boolean must be boolean (true/false)");
+                }
+                return null;
+            }
+        }
+        
 
         public static DateTime? DateTimeIsNotRequired(ReadOnlySpan<char> column)
-            => DateTime.TryParse(column, out DateTime columnValue) ? columnValue : null;
+        {
+            {
+                if (column.Length > 0)
+                {
+                    return DateTime.TryParse(column, out DateTime columnValue) ? columnValue
+                        : throw new CsvModelException("DateTime must be datetime");
+                }
+                return null;
+            }
+        }
 
         public static TimeSpan? TimeSpanIsNotRequired(ReadOnlySpan<char> column)
-            => TimeSpan.TryParse(column, out TimeSpan columnValue) ? columnValue : null;
+        {
+            {
+                if (column.Length > 0)
+                {
+                    return TimeSpan.TryParse(column, out TimeSpan columnValue) ? columnValue
+                        : throw new CsvModelException("DateTime must be datetime");
+                }
+                return null;
+            }
+        }
     }
 }
--- ReadCsvInDataTable/Location.cs
+++ ReadCsvInDataTable/Location.cs
@@ -1,8 +1,16 @@
-﻿namespace CsvReader
+﻿using System;
+
+namespace CsvReader
 {
-    public class Location : CsvAutoMapperModel
+    public class Location : CsvModel
     {
         public int Id { get; private set; }
         public string Name { get; private set; }
+
+        public override void InitializeFromRow(ReadOnlySpan<string> columns)
+        {
+            Id = IntegerIsRequired(columns[0]); // not for string (int, long, bool, datetime, timespan)
+            Name = columns[1]; // for string
+        }
     }
 }
