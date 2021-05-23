eb84b2e27f747d8cd0a338cdc12f8767a3edf904
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-23 15:57:00 +0300

    add xml serialization

---
+++ XmlSer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace XmlSer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var locationSer = new XmlSerializer(typeof(Location));
+
+            var location = new Location
+            {
+                Id = 1,
+                Name = "Test Location"
+            };
+
+            using var stringWriter = new StringWriter();
+            locationSer.Serialize(stringWriter, location);
+            Console.WriteLine(stringWriter.ToString());
+
+            using var stringReader = new StringReader(stringWriter.ToString());
+
+            var obj = locationSer.Deserialize(stringReader) as Location;
+            Console.WriteLine($"Location: {obj}");
+        }
+    }
+
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} {Name}";
+        }
+    }
+}
