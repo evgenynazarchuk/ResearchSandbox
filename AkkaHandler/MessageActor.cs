b4d40ca83cf8ca9bac9d083c3d053179b5bee1f9
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-05-29 16:54:39 +0300

    add code

---
+++ AkkaHandler/MessageActor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace AkkaHandler
+{
+    public class InformationMessage
+    {
+        private readonly string _message;
+        public string Message { get => _message; }
+
+        public InformationMessage(string message)
+        {
+            _message = message;
+        }
+    }
+
+    public class WarningMessage
+    {
+        private readonly string _message;
+        public string Message { get => _message; }
+
+        public WarningMessage(string message)
+        {
+            _message = message;
+        }
+    }
+
+    public class InformationMessageActor : ReceiveActor
+    {
+        public InformationMessageActor()
+        {
+            Receive<InformationMessage>(message =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(message.Message);
+                Console.ResetColor();
+            });
+        }
+    }
+
+    public class YellowMessageActor : ReceiveActor
+    {
+        public YellowMessageActor()
+        {
+            Receive<InformationMessage>(message =>
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(message.Message);
+                Console.ResetColor();
+            });
+        }
+    }
+
+    public class WarningMessageActor : ReceiveActor
+    {
+        public WarningMessageActor()
+        {
+            Receive<WarningMessage>(message =>
+            {
+                Console.Beep();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(message.Message);
+                Console.ResetColor();
+            });
+        }
+    }
+}
---
+++ AkkaHandler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using Akka;
+using Akka.Actor;
+
+namespace AkkaHandler
+{
+    class Program
+    {
+        static void Main()
+        {
+            ManyMessage();
+        }
+
+        static void ManyMessage()
+        {
+            const int CountInvoke = 100_000;
+            Stopwatch watch = new();
+
+            var system = ActorSystem.Create($"{nameof(Program)}");
+            var simpleMessage = system.ActorOf<InformationMessageActor>($"{nameof(InformationMessageActor)}");
+            var yellowMessage = system.ActorOf<YellowMessageActor>($"{nameof(YellowMessageActor)}");
+
+            watch.Start();
+            for (int i = 0; i < CountInvoke; i++)
+            {
+                simpleMessage.Tell(new InformationMessage("Hello world\r"));
+                yellowMessage.Tell(new InformationMessage("Hello world\r"));
+            }
+            watch.Stop();
+            var runTime = watch.ElapsedMilliseconds;
+
+            Console.ReadKey();
+            Console.WriteLine($"Watch: {runTime} ms");
+            Console.ReadKey();
+        }
+
+        static void CheckMessage()
+        {
+            var system = ActorSystem.Create($"{nameof(Program)}");
+            var actor1 = system.ActorOf<InformationMessageActor>($"{nameof(InformationMessageActor)}");
+            var actor2 = system.ActorOf<WarningMessageActor>($"{nameof(WarningMessageActor)}");
+            var actor3 = system.ActorOf<YellowMessageActor>($"{nameof(YellowMessageActor)}");
+
+            actor1.Tell(new InformationMessage("Hello world"));
+            actor2.Tell(new InformationMessage("Wow!")); // Print INFO, failed
+
+            actor2.Tell(new WarningMessage("Wow!"));
+
+            actor3.Tell(new InformationMessage("Bay"));
+
+            Console.ReadKey();
+        }
+    }
+}
