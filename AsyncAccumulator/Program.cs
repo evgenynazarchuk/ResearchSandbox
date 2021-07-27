1ab0cbb90108f22b50b90c35a9ee4482b77688ef
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-07-27 23:33:17 +0300

    test cuncurrency queue

---
+++ AsyncAccumulator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace AsyncAccumulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> queue = new();
+            List<int> list = new();
+            List<Task> tasks = new();
+
+            foreach(var i in Enumerable.Range(1, 10000))
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    queue.Enqueue(i);
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            foreach (var i in Enumerable.Range(1, 10000))
+            {
+                queue.TryDequeue(out int item);
+                list.Add(item);
+            }
+
+            list.Sort();
+
+            for (int i = 0; i < 10000 - 1; i++)
+            {
+                if (list[i] != list[i + 1] - 1)
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+    }
+}
--- DateTimeManage/Program.cs
+++ DateTimeManage/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace DateTimeManage
 {
@@ -6,16 +9,25 @@ namespace DateTimeManage
     {
         static void Main(string[] args)
         {
-            DateTime time = new DateTime();
-            Console.WriteLine(time);
-
-            PrintDateTime();
-            Console.WriteLine(DateTime.MinValue);
+            Stopwatch watch = new();
+            watch.Start();
+            Task.Delay(1000).Wait();
+            watch.Stop();
+            Console.WriteLine($"{watch.Elapsed}");
+            //var time1 = TimeSpan.FromMilliseconds(0.1d);
+            //Console.WriteLine(time1);
+            //var time2 = TimeSpan.FromMilliseconds(0.01d);
+            //Console.WriteLine(time2);
+            //DateTime time = new DateTime();
+            //Console.WriteLine(time);
+            //
+            //PrintDateTime();
+            //Console.WriteLine(DateTime.MinValue);
         }
 
-        static void PrintDateTime(DateTime time = new DateTime())
-        {
-            Console.WriteLine(time);
-        }
+        //static void PrintDateTime(DateTime time = new DateTime())
+        //{
+        //    Console.WriteLine(time);
+        //}
     }
 }
