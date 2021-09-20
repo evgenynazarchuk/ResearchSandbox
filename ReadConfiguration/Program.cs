f0bfc147e2387b77a2fbec17476481d92e33e550
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-09-20 21:32:23 +0300

    add

---
+++ ReadConfiguration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace ReadConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "ConnectionStrings:DefaultConnectionString", "ffffffffffffff" }
+            }).Build();
+
+            Console.WriteLine(config.GetConnectionString("Development"));
+        }
+    }
+}
